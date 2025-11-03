7c326547f8c52898e9c1b0e04e753d58360a8fa7
NikiHal013 <nikola.haluzova@unob.cz>
2025-11-03 23:04:47 +0100

    zaklad  1.0

---
+++ game.py
@@ -0,0 +1,61 @@
+import sys
+import pygame
+
+from scripts.utils import load_image, load_images
+from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption("Corebound - Demo")
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+
+        self.movement = [False, False]
+
+        self.assets = {
+            "decor": load_images("tiles/decor"),
+            "grass": load_images("tiles/grass"),
+            "large_decor": load_images("tiles/large_decor"),
+            "stone": load_images("tiles/stone"),
+            "player": load_image("entities/player.png"),
+        }
+
+        self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
+        self.tilemap = Tilemap(self, tile_size=16)
+
+    def run(self):
+        while True:
+            self.display.fill((27, 117, 51))
+
+            self.tilemap.render(self.display)
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[1] = False
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()))
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
